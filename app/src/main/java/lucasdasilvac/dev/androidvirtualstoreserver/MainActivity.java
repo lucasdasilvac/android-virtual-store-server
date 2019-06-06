f991cecba637d44126607c8199f801c39fc74818
Lucas Costa <lucascosta@alu.ufc.br>
2019-06-06 00:09:57 -0300

    add new category

--- app/src/main/java/lucasdasilvac/dev/androidvirtualstoreserver/MainActivity.java
+++ app/src/main/java/lucasdasilvac/dev/androidvirtualstoreserver/MainActivity.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignIn;
-    TextView txtSlogan;
+    //TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSignIn = (Button) findViewById(R.id.btnSignIn);
-        txtSlogan = (TextView) findViewById(R.id.txtSlogan);
+        //txtSlogan = (TextView) findViewById(R.id.txtSlogan);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
